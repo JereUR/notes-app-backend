d216c645e717f648b9291129678e71c05ad46fa3
JereUR <jeremias.jdv@gmail.com>
2024-08-12 21:51:54 -0300

    feat: Session configuration updated

--- types/express-session.d.ts
+++ types/express-session.d.ts
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose'
 import 'express-session'
+import mongoose from 'mongoose'
 
 declare module 'express-session' {
   interface SessionData {
-    userId: mongoose.Types.ObjectId
+    userId?: mongoose.Types.ObjectId
   }
 }
